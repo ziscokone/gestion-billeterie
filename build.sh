418c6af4cca23c2ecf2af6443396d80d44e04d22
zkone <zissongui.kone@veone.net>
2026-01-18 19:00:32 +0000

    Fix création superuser

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ python manage.py shell << EOF
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
+    User.objects.create_superuser('admin', 'admin123')
     print('Superuser créé avec succès')
 else:
     print('Superuser existe déjà')
