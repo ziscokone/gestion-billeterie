b9b28ad2b55a3ff2a544a7e6f3f1b2d81a54d9d3
zkone <zissongui.kone@veone.net>
2026-01-21 21:46:01 +0000

    Ajout de django.contrib.humanize pour le formatage des montants

--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',  # Pour les filtres de formatage (intcomma, etc.)
 
     # Local apps
     'core',
